8fe368988b611eb44d452e4ddbd456d10e766f42
Jonatan Pilenghy Pereira <jonapereira@gmail.com>
2021-11-03 07:22:44 -0300

    Bascit static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "assets/css/components/navBar";
+import Footer from "assets/css/components/Footer";
+import DataTable from "assets/css/components/DataTable";
+
 function App() {
   return (
-    <div>
-    <h1 className="text-primary"> Olá Mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary"> Olá Mundo</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
