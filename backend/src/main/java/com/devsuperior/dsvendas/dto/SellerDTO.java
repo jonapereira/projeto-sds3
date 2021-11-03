a4dfe92535bd75370843590d5aefaaffc4e147ac
Jonatan <jonapereira@gmail.com>
2021-11-03 14:51:35 -0300

    Sellers Maping

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SellerDTO implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long Id;
+	private String name;
+	
+	public SellerDTO() {
+		
+	}
+
+	public SellerDTO(Long id, String name) {
+		this.Id = id;
+		this.name = name;
+	}
+
+	public SellerDTO(Seller entity) {
+		Id = entity.getId();
+		this.name = entity.getName();
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
