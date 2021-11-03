0312e1b667094d9d879568c598179a73a7b9b9e7
Jonatan <jonapereira@gmail.com>
2021-11-03 14:13:32 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DsvendasApplication {
